2a49c2d14266777ef3c90f50ca6b261f2683b8ea
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-12-23 00:17:59 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,73 +1,30 @@
 import React from 'react';
-import api from './api.js';
 import RowMovie from './components/lists/RowMovie.js';
 import './App.css'
 import Featured from './components/featured/Featured.js';
 import Header from './components/header/Header.js';
 import Footer from './components/footer/Footer.js';
+import filmes from './api.js';
 
 
 const App = () => {
-  const [movieList, setMovieList] = React.useState([]);
-  const [featuredData, setFeaturedData] = React.useState(null);
-  const [blackHeader, setBlackHeader] = React.useState(false);
-
-  React.useEffect(() => {
-    const loadAll = async () => {
-
-      //testegrfdhgfghfjhgf
-
-      //pegando a lista TOTAL
-      let list = await api.getHomeList();
-      setMovieList(list);
-
-      //pegando o FEATURED
-
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-      let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await api.getMovieInfo(chosen.id, 'tv')
-
-
-      setFeaturedData(chosenInfo);
-    }
-
-    loadAll();
-  }, []);
-
-
-  React.useEffect(() => {
-    const scrollListener = () =>{
-      if (window.scrollY > 50) {
-        setBlackHeader(true);
-      } else {
-        setBlackHeader(false);
-      }
-    }
-
-    window.addEventListener('scroll', scrollListener);
-
-    return ()=>{
-    window.removeEventListener('scroll', scrollListener);
-    }
-  }, [])
-  
 
   return (
     <div className='page'>
-
-      <Header blackHeader={blackHeader}/>
-
-      {featuredData && <Featured item={featuredData} />}
+      <Header />
+      <Featured />
 
       <section className='lists'>
-        {movieList.map((item, key) => (
-          <RowMovie key={key} title={item.title} items={item.items} />
-        ))}
+        {filmes.map((i, key) => {
+          return <RowMovie
+            key={key}
+            title={i.title}
+            items={i.items}
+          />
+        })}
       </section>
 
-
-     <Footer />     
+      <Footer />
     </div>
 
   )
--- src/components/lists/RowMovie.js
+++ src/components/lists/RowMovie.js
@@ -2,20 +2,33 @@ import React from 'react'
 import './RowMovie.css'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import { getMovies } from '../../api';
 
 const RowMovie = ({ title, items }) => {
     const imageHost = "https://image.tmdb.org/t/p/";
     const [scrollX, setScrollX] = React.useState(0)
+    const [movieList, setMovieList] = React.useState([]);
+
+    const fetchMovies = async (_path) => {
+        try {
+            const data = await getMovies(_path);
+            setMovieList(data?.results)
+        } catch (error) { console.log('deu erro no fetch', error)}
+    }
+
+    React.useEffect(() => {
+        fetchMovies(items)
+    }, [items]);
 
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2.5);
-        if(x > 0) x = 0; 
+        if (x > 0) x = 0;
         setScrollX(x);
     }
     const handleRightArrow = () => {
         let x = scrollX - Math.round(window.innerWidth / 2.5);
-        let listSize = items.results.length * 150;
-        if((window.innerWidth - listSize) > x){
+        let listSize = movieList.length * 150;
+        if ((window.innerWidth - listSize) > x) {
             x = (window.innerWidth - listSize) - 60;
         }
         setScrollX(x);
@@ -28,17 +41,16 @@ const RowMovie = ({ title, items }) => {
             <div className='movieRow--left' onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{ fontSize: 50 }} />
             </div>
-
-            <div className='movieRow--right' onClick={handleRightArrow}>
+            <div className='movieRow--right' onClick={handleRightArrow} >
                 <NavigateNextIcon style={{ fontSize: 50 }} />
             </div>
 
             <div className='movieRow--listarea'>
                 <div className='movieRow--list' style={{
                     marginLeft: scrollX,
-                    width: items.results.length * 150
+                    width: movieList.length * 150,
                 }}>
-                    {items.results.length > 0 && items.results.map((item, key) => (
+                    {movieList?.map((item, key) => (
                         <div className='movieRow--item' key={key}>
                             <img src={`${imageHost}w300${item.poster_path}`} alt={item.title} />
                         </div>
